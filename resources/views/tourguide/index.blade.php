4e8d1d4bb99ddb9924432f1d3e28f49b5b552ef0
Basty <bastygottocollege@gmail.com>
2024-05-15 16:32:46 +0800

    Tourguide

--- resources/views/tourguide/index.blade.php
+++ resources/views/tourguide/index.blade.php
@@ -2,8 +2,21 @@
 
 @section('content')
     <div class="container">
-        <div class="d-flex justify-content-center m-5">
-            <h2>Tourguide</h2>
+        <div class="text-center justify-content-center m-5 hero">
+            <h2 class="hero-text">Tour Guide</h2>
+        </div>
+        <div class="content">
+            <ul class="nav justify-content-center isotope-options mb-60 pb-30">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">apple</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">banana</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">cherry</a>
+                </li>
+            </ul>
         </div>
     </div>
 @endsection
